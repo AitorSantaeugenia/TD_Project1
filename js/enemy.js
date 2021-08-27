0a6c1a933b9796ba357dad3b2e6c32c289550c0d
AitorSantaeugenia <aitor.j.s.m@gmail.com>
2021-08-27 20:21:00 +0200

    sprites added, wip sprites and other changes

--- js/enemy.js
+++ js/enemy.js
@@ -28,8 +28,6 @@ class SmallDemon extends Enemy {
 		this.x = this.path[0][0];
 		this.y = this.path[0][1];
 		this.posInicialX = this.x;
-		this.w = 50;
-		this.h = 50;
 		this.wHPBar = 35;
 		this.pathIndex = 0;
 		this.slow = false;
@@ -40,15 +38,55 @@ class SmallDemon extends Enemy {
 		// Imagen enemigo red.png
 		this.randomImage = Math.trunc(Math.random() * 8);
 		this.img = new Image();
-		this.img.src = `https://aitorsantaeugenia.github.io/TD_Project1/images/enemies/enemy${this.randomImage}.png`;
+		this.img.src = `../images/enemies/enemy${this.randomImage}.png`;
+
+		//tring out spritesheets
+		this.w = 50;
+		this.h = 50;
+		//https://dev.to/martyhimmel/animating-sprite-sheets-with-javascript-ag3
+		//Work in progress
+		//1- pillar posicion imagen
+		//2- pintar imagen (acordarse del random de arriba)
+		//3- animar con frames
+		// this.scale = 2;
+		// this.scaledWidth = this.scale * this.w;
+		// this.scaledHeight = this.scale * this.h;
+		// this.cycleLoop = [ 0, 1, 2 ];
+		// this.currentLoopIndex = 0;
+		// this.frameCount = 0;
 	}
 
 	draw() {
 		this.context.drawImage(this.img, this.x - this.w / 2, this.y - this.h / 2, this.w, this.h);
 		this.paintHpBar();
+		// this.context.ctx.drawImage(
+		// 	this.img,
+		// 	this.frameX * this.w,
+		// 	this.frameY * this.h,
+		// 	this.w,
+		// 	this.h,
+		// 	this.canvasX,
+		// 	this.canvasY,
+		// 	this.scaledWidth,
+		// 	this.scaledHeight
+		// );
 	}
 
 	move() {
+		// this.frameCount++;
+		// if (this.frameCount < 15) {
+		// 	window.requestAnimationFrame(step);
+		// 	return;
+		// }
+		// this.frameCount = 0;
+		// this.ctx.clearRect(0, 0, this.canvas.w, this.canvas.h);
+		// drawFrame(this.cycleLoop[this.currentLoopIndex], 0, 0, 0);
+		// this.currentLoopIndex++;
+		// if (this.currentLoopIndex >= this.cycleLoop.length) {
+		// 	this.currentLoopIndex = 0;
+		// }
+		// window.requestAnimationFrame(step);
+
 		this.directionPath();
 	}
 
